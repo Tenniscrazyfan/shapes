fb6e2f29e4a5158f62de8d9ff0a859aad98f7b9b
Tenniscrazyfan <diyappatil17@gmail.com>
2025-09-06 09:30:13 +0100

    Update shapes.py

--- shapes.py
+++ shapes.py
@@ -17,32 +17,43 @@ rect1 = Rectangle("Black", (100,100,80,70))
 
 
 class Circle():
-    def __init__(self,color,radius,c):
+    def __init__(self,color,radius,c,thickness):
         self.screen = screen
         self.color = color
         self.radius = radius
         self.c = c
+        self.thickness = thickness
 
     def draw(self):
-        pygame.draw.circle(self.screen,self.color,self.c,self.radius,0)
+        pygame.draw.circle(self.screen,self.color,self.c,self.radius,self.thickness)
 
     def grow(self):
         self.radius += 10
-        pygame.draw.circle(self.screen,self.color,self.c,self.radius,0)
+        pygame.draw.circle(self.screen,self.color,self.c,self.radius,self.thickness)
 
-circle1 = Circle("Black", 90 ,(100,190))    
+circle3 = Circle("teal", 50,(500,190), 70)
+circle1 = Circle("red", 100 ,(500,190), 0)   
+
+screen.fill("PaleGreen")
 
 
 while playing :
     for event in pygame.event.get():
+        if event.type == pygame.MOUSEMOTION:
+            circle2 = Circle("white", 10,pygame.mouse.get_pos(),0)
+            circle2.draw()
+            pygame.display.update()
         if event.type == pygame.MOUSEBUTTONDOWN:
+            circle3.grow()
             circle1.grow()
             pygame.display.update()
         if event.type == pygame.QUIT:
             playing=False
             pygame.quit()
-        
-    screen.fill("PaleGreen")
-    rect1.draw()
+
     circle1.draw()
-    pygame.display.update()
+    circle3.draw()
+        
+    
+
+    
